00291da551d2f4f6204c4c23935cd6c32af199e5
Cyrus Vachha <cvachha549@gmail.com>
2025-12-12 02:16:00 -0500

    Adding splat previews for results

--- static/js/index.js
+++ static/js/index.js
@@ -65,13 +65,14 @@ $(document).ready(function() {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
-    preloadInterpolationImages();
+    // Commented out - interpolation images don't exist and aren't used in the current HTML
+    // preloadInterpolationImages();
 
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    // $('#interpolation-slider').on('input', function(event) {
+    //   setInterpolationImage(this.value);
+    // });
+    // setInterpolationImage(0);
+    // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
     bulmaSlider.attach();
 
